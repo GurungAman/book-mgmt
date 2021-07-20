609df247e2e91d31c3a7603587af1faae5e8e8e1
Aman Gurung <amenag30@gmail.com>
2021-07-20 13:32:34 +0545

    requiremnets file added

--- bookManagement/settings.py
+++ bookManagement/settings.py
@@ -98,6 +98,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'chatapp',
+#         'USER': 'amangrg',
+#         'PASSWORD': 'amangrg123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
