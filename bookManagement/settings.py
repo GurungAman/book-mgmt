3c2f3c23b852f5b8e180294e1f4436a6f3c61efc
Aman Gurung <amenag30@gmail.com>
2021-07-20 21:17:57 +0545

    fixed update data not showing up

--- bookManagement/settings.py
+++ bookManagement/settings.py
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'bookManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -146,5 +146,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import django_heroku
-django_heroku.settings(locals()) 
+# import django_heroku
+# django_heroku.settings(locals()) 
